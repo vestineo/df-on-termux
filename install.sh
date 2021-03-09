94e06a7146704db5e63c1f7f2631b475e7269514
Prashant Nagar <vestineo@gmail.com>
2021-03-09 13:46:12 +0530

    chmod install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ git clone git://github.com/vestineo/df-on-termux.git
 mkdir -p /data/data/com.termux/files/usr/opt/
 mv /data/data/com.termux/files/home/df-on-termux /data/data/com.termux/files/usr/opt/df_sandbox
 mv /data/data/com.termux/files/usr/opt/df_sandbox/dwarf-fortress /data/data/com.termux/files/usr/bin/
+chmod +x /data/data/com.termux/files/usr/bin/dwarf-fotress
