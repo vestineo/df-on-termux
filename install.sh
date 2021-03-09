08ee54a1295a6bd176f5a37139085f128288650d
Prashant Nagar <vestineo@gmail.com>
2021-03-09 13:43:28 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash -e
 cd /data/data/com.termux/files/home
 apt update
-apt install git proot unstable-repo
-apt install qemu-x86_64
+apt -y install git proot 
+apt -y install unstable-repo && apt -y install qemu-x86_64
 git clone git://github.com/vestineo/df-on-termux.git
 mkdir -p /data/data/com.termux/files/usr/opt/
 mv /data/data/com.termux/files/home/df-on-termux /data/data/com.termux/files/usr/opt/df_sandbox
