2aebd314821cdb32aabf14d9cb2f3ac5608e3f56
Prashant Nagar <vestineo@gmail.com>
2021-09-16 09:45:56 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ printf "${red}\n"
 printf " >> ${yellow} Updating apt cache...\n"
 apt update -y
 printf "${red} >> ${yellow} Installing all dependencies ...\n"
-for i in git proot unstable-repo qemu-user-x86_64; do
+for i in git proot unstable-repo qemu-user-x86-64; do
 		echo "       Installing ${i}..."
 		apt install -y $i || {
 			printf "$red"
@@ -25,7 +25,7 @@ printf "${yellow}\n"
 git clone git://github.com/vestineo/df-on-termux.git
 mkdir -p /data/data/com.termux/files/usr/opt/
 mv /data/data/com.termux/files/home/df-on-termux /data/data/com.termux/files/usr/opt/df_sandbox
-mv /data/data/com.termux/files/usr/opt/df_sandbox/{dwarf-fortress, df-switchfont} /data/data/com.termux/files/usr/bin/
+mv /data/data/com.termux/files/usr/opt/df_sandbox/dwarf-fortress /data/data/com.termux/files/usr/bin/
 printf "${red} [!] ${yellow} To play the game, use ${blue} dwarf-fortress ${yellow} command \n"
 printf "${red} [!] ${yellow} To change game font, use ${blue} df-switchfont ${yellow} command \n"
 printf "${reset}"
